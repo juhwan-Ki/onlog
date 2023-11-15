8a72de154303472ee7887079c4122ce9a84ae09f
juhwan <kjhw990218@gmail.com>
2023-11-15 23:16:37 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/onlog/OnlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
